da37ef38955e60327ad4c70bffc488ad714368a8
Maksim Vdovin <vdvmaxim@gmail.com>
2019-07-02 00:15:14 +0500

    29 доделал

--- JavaScript30-master/29 - Countdown Timer/scripts-START.js
+++ JavaScript30-master/29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,15 @@
 let countdown;
-
-
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+// const form = document.querySelector
 
 function timer(seconds) {
+    clearInterval(countdown);
     const now = Date.now();
     const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
     // console.log({now, then});
 
     countdown = setInterval(() => {
@@ -12,9 +17,40 @@ function timer(seconds) {
 
         if (secondLeft <= 0) {
             clearInterval(countdown);
-            // 5-57
             return;
         }
-        console.log(secondLeft);
+        displayTimeLeft(secondLeft);
     }, 1000);
 }
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0': '' }${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
+
+    // console.log({minutes, remainderSeconds});
+} 
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be back At ${adjustedHour}:${minutes < 10 ? '0': ''}${minutes}`;
+}
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+
+document,customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins*60);
+    this.reset();
+});
